04fca47b97541c7cb27742b1ccb0ba11172c9997
daltonlin <>
2019-12-16 20:59:56 -0800

    add time_elapsed funcation to timestamp models

--- carbon_offset/timestamp/models.py
+++ carbon_offset/timestamp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 
 class Start(models.Model):
@@ -7,7 +8,9 @@ class Start(models.Model):
     user_name = models.CharField(max_length=100)#project's name
     time_started = models.DateTimeField(auto_now_add=True) #auto_now_add=True to ensure that the start time data is unable to be modified
 
-class
+    def time_elapsed(self):
+        diff = datetime.now() - self.time_started
+        return diff.total_seconds() / (60 * 60)
 
 
 
